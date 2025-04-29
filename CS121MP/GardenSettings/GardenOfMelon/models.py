b95978c68b265b8d2a43ce28f40f3bb4f0d94ea5
@louisruemyr <louisruemyr@gmail.com>
2025-04-29 14:00:11 +0800

    Made migrations for customer class

--- CS121MP/GardenSettings/GardenOfMelon/models.py
+++ CS121MP/GardenSettings/GardenOfMelon/models.py
@@ -39,6 +39,16 @@ class User(AbstractUser):
     
     def __str__(self):
         return self.email
+    
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+
 
 class ContactMessage(models.Model):
     name = models.CharField(max_length=255)
