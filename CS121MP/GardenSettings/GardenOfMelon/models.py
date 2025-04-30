671d4785ca694a120d7da6a953e5136bfc453a3c
Lance Regala <lgregala@up.edu.ph>
2025-04-30 02:41:34 -0700

    Updated Partial Cart Features and Login Authentication

--- CS121MP/GardenSettings/GardenOfMelon/models.py
+++ CS121MP/GardenSettings/GardenOfMelon/models.py
@@ -13,16 +13,13 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
 
-
         if not password:
             raise ValueError('Superusers must have a password.')
-
 
         return self.create_user(email, password, **extra_fields)
 
@@ -33,14 +30,13 @@ class User(AbstractUser):
 
     # Use email as the login identifier instead of username
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number']  
+    REQUIRED_FIELDS = ['first_name', 'last_name']  
 
     objects = CustomUserManager()
     
     def __str__(self):
         return self.email
     
-
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
@@ -48,7 +44,6 @@ class Customer(models.Model):
     
     def __str__(self):
         return self.name
-
 
 class ContactMessage(models.Model):
     name = models.CharField(max_length=255)
@@ -70,10 +65,11 @@ class Product(models.Model):
     digital = models.BooleanField(default=False, null=True, blank=False)  # Optional, if digital products are part of store
     image = models.ImageField(null=True, blank=True)
 
-
     def __str__(self):
         return self.name
-
+    
+    def imageURL(self):
+        return self.image.url
 
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
@@ -81,9 +77,20 @@ class Order(models.Model):
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
 
-
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total 
+    
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total 
    
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
@@ -91,6 +98,10 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
@@ -100,7 +111,6 @@ class ShippingAddress(models.Model):
     state = models.CharField(max_length=200, null=True)
     zipcode = models.CharField(max_length=200, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
-
 
     def __str__(self):
         return self.address
