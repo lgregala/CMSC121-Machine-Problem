b3b94650d5a82f96afff6912991edeedff8aa191
Lance Regala <lgregala@up.edu.ph>
2025-05-12 02:36:15 -0700

    Added automatic seller to all products

--- CS121MP/GardenSettings/GardenOfMelon/admin.py
+++ CS121MP/GardenSettings/GardenOfMelon/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from .models import *
 
+# Manually set the current admin as the seller of the product
+class ProductAdmin(admin.ModelAdmin):
+    # Make it non-editable
+    readonly_fields = ('seller',)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.seller:
+            obj.seller = request.user
+        obj.save()
 
 admin.site.register(User)
 admin.site.register(Customer)
 admin.site.register(ContactMessage)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
--- CS121MP/GardenSettings/GardenOfMelon/signals.py
+++ CS121MP/GardenSettings/GardenOfMelon/signals.py
@@ -3,16 +3,17 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Customer
 
-# All functions below this line automatically creates a 'Customer' for every signed up 'User'
+# Signal to create a 'Customer' when a new 'User' is created
 @receiver(post_save, sender=User)
 def create_customer(sender, instance, created, **kwargs):
     if created:
         Customer.objects.create(
             user=instance,
             name=instance.username,
-            email=instance.email
+            email=instance.email,
         )
 
+# Signal to save the 'Customer' instance whenever the 'User' instance is saved
 @receiver(post_save, sender=User)
 def save_customer(sender, instance, **kwargs):
     if hasattr(instance, 'customer'):
