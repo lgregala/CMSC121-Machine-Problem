7737ab2f999051afa7ff73977eed92f264581f01
@louisruemyr <louisruemyr@gmail.com>
2025-05-12 23:55:34 +0800

    Edited Admin

--- CS121MP/GardenSettings/GardenOfMelon/admin.py
+++ CS121MP/GardenSettings/GardenOfMelon/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from .models import *
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'scientific_name', 'category', 'subcategory', 
+                    'description', 'difficulty', 'light_needed', 'watering_schedule', 
+                    'price', 'quantity']
+
 
 admin.site.register(User)
 admin.site.register(Customer)
 admin.site.register(ContactMessage)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
